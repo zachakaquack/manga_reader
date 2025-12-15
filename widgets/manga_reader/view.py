5b32f631e7f740b313c9b94fd8c95bc78cbc5851
zachakaquack <zach12399@gmail.com>
2025-12-14 20:30:32 -0600

    page navigation

--- widgets/manga_reader/view.py
+++ widgets/manga_reader/view.py
@@ -1,12 +1,16 @@
-from PySide6.QtGui import Qt
+from PySide6.QtGui import QKeyEvent, Qt
 from PySide6.QtWidgets import QFrame, QSizePolicy, QVBoxLayout
 from settings.loader import load_settings
 from widgets.general.reader_top_bar import ReaderTopBar
-
+from pathlib import Path
+from widgets.manga_reader.page import Page
 
 class MangaReaderView(QFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, *kwargs)
+
+        self.page_number_index: int = 0
+        self.image_paths: list[Path] = []
 
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
@@ -30,7 +34,51 @@ class MangaReaderView(QFrame):
             Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter
         )
 
-
         self.top_bar = ReaderTopBar()
         self.main_layout.addWidget(self.top_bar)
 
+        self.page = Page()
+        self.main_layout.addWidget(self.page)
+
+    def keyPressEvent(self, event: QKeyEvent, /) -> None:
+
+        match event.key():
+            case Qt.Key.Key_H:
+                self._prev_page()
+            case Qt.Key.Key_A:
+                self._prev_page()
+
+            case Qt.Key.Key_L:
+                self._next_page()
+            case Qt.Key.Key_D:
+                self._next_page()
+
+
+        return super().keyPressEvent(event)
+
+    def _next_page(self):
+        if(self.page_number_index >= len(self.image_paths) - 1):
+            return
+
+        self.page_number_index += 1
+        self.top_bar.page.update_current(self.page_number_index + 1)
+
+        self.page.change_page(self.image_paths[self.page_number_index])
+
+    def _prev_page(self):
+        if(self.page_number_index <= 0):
+            return
+
+        self.page_number_index -= 1
+        self.top_bar.page.update_current(self.page_number_index + 1)
+
+        self.page.change_page(self.image_paths[self.page_number_index])
+
+    def load_manga(self, image_paths: list[Path]) -> None:
+        if(len(image_paths) < 1):
+            raise IndexError("Too few images:", len(image_paths))
+
+        self.top_bar.page.update_limit(len(image_paths))
+
+        self.image_paths = image_paths
+        self.page.change_page(image_paths[0])
