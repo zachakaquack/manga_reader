cc123db966e61e7b2654dc37692d80837e7d6c33
zachakaquack <zach12399@gmail.com>
2025-12-14 21:30:53 -0600

    side bar added

---
+++ widgets/reader/side_bar.py
@@ -0,0 +1,37 @@
+from PySide6.QtGui import Qt
+from PySide6.QtWidgets import QFrame, QLabel, QSizePolicy, QVBoxLayout
+
+from settings.loader import load_settings
+
+
+class SideBar(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+
+        settings = load_settings()
+        self.setFixedWidth(settings.side_bar.width)
+
+        self.setSizePolicy(
+            QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Expanding
+        )
+
+        self.setObjectName("reader_side_bar")
+        self.setStyleSheet(
+            f"""
+            #reader_side_bar{{
+                background-color: {settings.colors.reader_top_bar_background_color};
+                color: {settings.colors.main_text_color};
+            }}
+            QLabel{{
+                color: {settings.colors.main_text_color};
+            }}
+
+            """
+        )
+
+        self.main_layout = QVBoxLayout(self)
+        self.setLayout(self.main_layout)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_layout.setSpacing(0)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)
+
