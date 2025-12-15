3fcb4c48a5798cb7b689116c8aa8b25c91330880
zachakaquack <zach12399@gmail.com>
2025-12-14 19:54:56 -0600

    main interface

---
+++ widgets/interface.py
@@ -0,0 +1,35 @@
+from PySide6.QtGui import Qt
+from PySide6.QtWidgets import QFrame, QSizePolicy, QVBoxLayout
+
+from settings.loader import load_settings
+from widgets.manga_reader.view import MangaReaderView
+
+
+class Interface(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
+        settings = load_settings()
+        self.setObjectName("interface")
+        self.setStyleSheet(
+            f"""
+            #interface{{
+                background-color: {settings.colors.main_background_color};
+                color: {settings.colors.main_text_color};
+            }}
+
+            """
+        )
+
+        self.main_layout = QVBoxLayout(self)
+        self.setLayout(self.main_layout)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_layout.setSpacing(0)
+        self.main_layout.setAlignment(
+            Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter
+        )
+
+        self.manga_view = MangaReaderView()
+        self.main_layout.addWidget(self.manga_view)
