99ce5a53c7e868a8117442879eba8a928d14ab98
zachakaquack <zach12399@gmail.com>
2025-12-14 21:27:19 -0600

    added sidebar

--- settings/reader.py
+++ settings/reader.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class Reader:
     # GENERAL
-    reader_top_bar_height: int = 50
+    top_bar_height: int = 50
 
     # COLORS
-    reader_top_bar_background_color: str = "#303030"
+    top_bar_background_color: str = "#303030"
--- settings/settings.py
+++ settings/settings.py
@@ -2,9 +2,11 @@ from dataclasses import dataclass, field
 
 from settings.colors import Colors
 from settings.reader import Reader
+from settings.side_bar import SideBar
 
 @dataclass
 class Settings:
     # GENERAL
     reader: Reader = field(default_factory=Reader)
+    side_bar: SideBar = field(default_factory=SideBar)
     colors: Colors = field(default_factory=Colors)
