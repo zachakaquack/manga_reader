d24e51b0a5849916f5600ae23e2510ecf2a2a04e
zachakaquack <zach12399@gmail.com>
2025-12-15 13:24:33 -0600

    switch to setUpClass to only run create_images once

--- test/test_scroller_reader.py
+++ test/test_scroller_reader.py
@@ -1,17 +1,24 @@
 import unittest
+import copy
 from pathlib import Path
 from widgets.reader.scroller.scroll_model import ScrollerModel
 
 class TestScrollerReaderModel(unittest.TestCase):
-    def setUp(self) -> None:
-        self.model = ScrollerModel()
-        self.model.image_paths = [
-                Path("/home/zach/Desktop/python/reader/assets/test/SMALL_scroller_page.png"),
-                Path("/home/zach/Desktop/python/reader/assets/test/SMALL_scroller_page.png"),
-                Path("/home/zach/Desktop/python/reader/assets/test/SMALL_scroller_page.png"),
+    @classmethod
+    def setUpClass(cls):
+        cls._base_model = ScrollerModel()
+        cls._base_model.image_paths = [
+            Path("/home/zach/Desktop/python/reader/assets/test/SMALL_scroller_page.png"),
+            Path("/home/zach/Desktop/python/reader/assets/test/SMALL_scroller_page.png"),
+            Path("/home/zach/Desktop/python/reader/assets/test/SMALL_scroller_page.png"),
+            Path("/home/zach/Desktop/python/reader/assets/test/SMALL_scroller_page.png"),
+            Path("/home/zach/Desktop/python/reader/assets/test/SMALL_scroller_page.png"),
         ]
-        self.model.page_number_index = 1
-        self.model.create_images()
+        cls._base_model.page_number_index = 2
+        cls._base_model.create_images()
+
+    def setUp(self):
+        self.model = copy.copy(self._base_model)
 
     def test_incrementPageNumberPixelsScrolledScrollerReader(self):
         # next_page returns the pixels to scroll in the qscrollarea
@@ -20,6 +27,7 @@ class TestScrollerReaderModel(unittest.TestCase):
         # pixels should be (index + 1) * 5000
         one_image_height = self.model._images_scaled[0].height()
         target_pixels = one_image_height * (self.model.page_number_index + 1)
+        print(one_image_height, target_pixels)
         self.assertEqual(target_pixels, self.model.next_page())
 
     def test_decrementPageNumberPixelsScrolledScrollerReader(self):
@@ -34,7 +42,7 @@ class TestScrollerReaderModel(unittest.TestCase):
     def test_incrementPageNumberInvalidScrollerReader(self):
         # the image's height gets scaled up to 5000 because 200 * 5 = 1000 (1000 is goal width)
         # pixels should be (index - 1) * 5000
-        self.model.page_number_index = 2
+        self.model.page_number_index = 4
         one_image_height = self.model._images_scaled[0].height()
 
         # target is the same, because we can't skip forward any pages
