8a78d6bb31d7467c6167b55c6e61ac8404c68025
zachakaquack <zach12399@gmail.com>
2025-12-16 12:27:01 -0600

    new settings for bottom bar

--- settings/colors.py
+++ settings/colors.py
@@ -3,11 +3,15 @@ from dataclasses import dataclass
 @dataclass
 class Colors:
     # main
-    main_background_color: str = "#1e1e1e"
-    main_text_color: str = "#eeeeee"
+    main_background: str = "#1e1e1e"
+    main_text: str = "#eeeeee"
 
     # reader
-    reader_top_bar_background_color: str = "#303030"
+    reader_top_bar_background: str = "#303030"
 
     # buttons
-    button_background_color = "#404040"
+    button_background = "#404040"
+
+    # bottom bar
+    bottom_bar_background = "#303030"
+    bottom_bar_filled_in = "#505050"
--- settings/settings.py
+++ settings/settings.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 
+from settings.bottom_bar import BottomBar
 from settings.colors import Colors
 from settings.reader import Reader
 from settings.side_bar import SideBar
@@ -9,4 +10,5 @@ class Settings:
     # GENERAL
     reader: Reader = field(default_factory=Reader)
     side_bar: SideBar = field(default_factory=SideBar)
+    bottom_bar: BottomBar = field(default_factory=BottomBar)
     colors: Colors = field(default_factory=Colors)
