22b4e5aeb6cbed1f21a3ea3d3cfb569d10aa69dc
zachakaquack <zach12399@gmail.com>
2025-12-17 08:58:09 -0600

    page changer on sidebar

---
+++ widgets/reader/side_bar/page_changer.py
@@ -0,0 +1,45 @@
+from PySide6.QtCore import Signal
+from PySide6.QtGui import Qt
+from PySide6.QtWidgets import QFrame, QHBoxLayout, QSizePolicy
+from settings.loader import load_settings
+from widgets.components.button import Button
+
+
+class PageChanger(QFrame):
+
+    increment = Signal()
+    decrement = Signal()
+    switch_to_big_view = Signal(str)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+
+        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.main_layout = QHBoxLayout(self)
+        self.setLayout(self.main_layout)
+        self.main_layout.setContentsMargins(2, 2, 2, 2)
+        self.main_layout.setSpacing(2)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        settings = load_settings()
+
+        decrement = Button("<")
+        page_button = Button("Page 123")
+        increment = Button(">")
+
+        decrement.clicked.connect(self.decrement)
+        page_button.clicked.connect(lambda: self.switch_to_big_view.emit("page"))
+        increment.clicked.connect(self.increment)
+
+        # TODO: maybe change to not be fixed? has to be fixed due
+        # to stupid layout behavior with qpushbuttons
+        decrement.setFixedWidth(settings.side_bar.arrow_button_fixed_width)
+        increment.setFixedWidth(settings.side_bar.arrow_button_fixed_width)
+
+        for button in [decrement, page_button, increment]:
+            button.setFixedHeight(settings.side_bar.button_height)
+            button.setSizePolicy(
+                QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+            )
+            self.main_layout.addWidget(button)
