a1398a980da7acc27ff41ef9b8e29149d5e2cbee
zachakaquack <zach12399@gmail.com>
2025-12-17 09:54:31 -0600

    colors for sidebar

--- settings/colors.py
+++ settings/colors.py
@@ -10,6 +10,10 @@ class Colors:
     # reader
     reader_top_bar_background: str = "#303030"
 
+    # side bar
+    side_bar_top_bar_background: str = "#303030"
+    side_bar_button_active_chapter: str = "#505050"
+
     # buttons
     button_background = "#404040"
 
