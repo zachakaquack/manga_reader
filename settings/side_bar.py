c36716fdec036d6fc5c6ee5b626f59d0ebd80756
zachakaquack <zach12399@gmail.com>
2025-12-17 09:54:48 -0600

    settings for button height in list view

--- settings/side_bar.py
+++ settings/side_bar.py
@@ -7,6 +7,7 @@ class SideBar:
     width: int = 250
     button_height: int = 50
     arrow_button_fixed_width: int = 32
+    list_view_button_fixed_height: int = 40
 
     # COLORS
     background_color: str = "#303030"
